4e1f74599c0c388c5ea05a58da15f1b20684525e
Yuuki Yamashita <mikufuworks@gmail.com>
2017-11-12 17:31:46 +0900

    Changed sample code

--- main.lua
+++ main.lua
@@ -2,9 +2,13 @@ require "love9slice"
 
 function love.load()
     img1 = Image9Slice:new("test.png", 10, 10, 38, 38)
+    img2 = Image9Slice:new("test.png", 10, 10, 38, 38)
+
     img1:resize(240, 120)
+    img2:resizeScale(1.5, 3)
 end
 
 function love.draw()
     img1:draw(10, 10)
+    img2:draw(10, 150)
 end
